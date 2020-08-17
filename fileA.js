787d360ee5df44f43f77f87e5e6a8a37c84933c6
Rebecca Amarh-Adjei <37591664+ramarhadjei@users.noreply.github.com>
2020-08-16 21:32:41 -0400

    first commit

---
+++ fileA.js
@@ -0,0 +1,9 @@
+//const exportsFromFileB = require('./fileB') // note the relative file path
+//console.log(exportsFromFileB)
+//console.log(exportsFromFileB.foo )
+//console.log(exportsFromFileB.bar)
+
+const exportsFromFileB = require('./fileB')
+exportsFromFileB()
+require('./fileB')
+require('./fileB')//not a typo-actually require it twice!
